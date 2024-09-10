ea1a14cf97137877e8e2218472e46ec76b3f9f21
Ahliyat <ahliyahorenuga@gmail.com>
2024-09-10 14:30:37 +0100

    javascript changes

---
+++ app.js
@@ -0,0 +1,68 @@
+let projectDetail = document.getElementById("project");
+const data = [
+  {
+    id: 1,
+    img: "./assets/images/portfolio img.jpg",
+    title: "Lorem, ipsum dolor.",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus voluptates inventore dolore animi vero cumque! ",
+  },
+  {
+    id: 2,
+    img: "./assets/images/portfolio img.jpg",
+    title: "Lorem, ipsum dolor.",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus voluptates inventore dolore animi vero cumque! ",
+  },
+  {
+    id: 3,
+    img: "./assets/images/portfolio img.jpg",
+    title: "Lorem, ipsum dolor.",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus voluptates inventore dolore animi vero cumque! ",
+  },
+  {
+    id: 4,
+    img: "./assets/images/portfolio img.jpg",
+    title: "Lorem, ipsum dolor.",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus voluptates inventore dolore animi vero cumque! ",
+  },
+  {
+    id: 5,
+    img: "./assets/images/portfolio img.jpg",
+    title: "Lorem, ipsum dolor.",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus voluptates inventore dolore animi vero cumque! ",
+  },
+];
+console.log(data);
+const fetchProjects = () => {
+  let projects = "";
+  data.map((project) => {
+    projects += `
+      
+          <section class="project-content">
+            <img src="${project.img}" alt="" />
+            <h3 class="project-title">${project.title}</h3>
+            <p class="project-desc">
+            ${project.description} 
+            </p>
+            <button class="project-btn">View Project</button>
+          </section>
+     `;
+  });
+
+  projectDetail.innerHTML = projects;
+};
+// projectDetail.style.display = "flex"
+
+//   projectDetail.style.gap = "2rem"
+//   projectDetail.style.flexWrap = "wrap"
+//   projectDetail.style.justifyContent = "center"
+//   projectDetail.style.alignItems = "center"
+//   projectDetail.style.marginTop = "2rem"
+//   projectDetail.style.marginBottom = "5rem"
+
+fetchProjects();
+
